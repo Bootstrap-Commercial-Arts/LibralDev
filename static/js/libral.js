fb1f8ad976a24254b64d4812503f168dac450a96
bythebootstrap <schad@bythebootstrap.us>
2023-05-08 13:56:39 -0700

    fixed cartLineUrls

--- static/js/libral.js
+++ static/js/libral.js
@@ -4,6 +4,24 @@ let main = document.getElementById('main');
 
 const setAndProductProjection = `_type == 'set' => {_id, _type, 'image': image.asset->url, title, 'slug': slug.current}, _type == 'product' => {_id, _type, louLink, louText, primary->, 'shopifyId': store.id, 'image': store.previewImageUrl, 'slug': store.slug.current, 'title': store.title, store }`;
 
+
+// Cart structure check
+let cartStructureCheck = function(cartObject){
+    if(cartObject.lines.edges){
+      cartRoot = cartObject.lines.edges;
+    } else if (cartObject.lines.nodes){
+      cartRoot = cartObject.lines.nodes;
+    } else {
+      topBannerStart('error','There is an error in your cart, please clear your browser\'s cache and try again');
+    }
+  };
+  
+// libralCart fill
+if(sessionStorage.libralCart){
+    libralCart = JSON.parse(sessionStorage.libralCart);
+    cartStructureCheck(libralCart);
+};
+
 // Search icon
 const searchForm = document.forms.searchBar
 function searchBarSubmit(event){
@@ -26,6 +44,24 @@ function replaceLast(find, replace, string) {
   var endString = string.substring(lastIndex + find.length);
   return beginString + replace + endString;
 }
+
+// Cart Icon Quantity display
+function cartIconQty() {
+    if(libralCart) {
+      if(document.getElementById("cart-icon-quantity")){
+        var cartQty = document.getElementById("cart-icon-quantity")
+        cartQty.innerHTML = cartRoot.length;
+      } else {
+        var cartIcon = document.getElementById('cart-icon');
+        var cartQty = document.createElement('p');
+        cartQty.setAttribute("id", "cart-icon-quantity");
+        cartQty.innerHTML = cartRoot.length;
+        cartIcon.append(cartQty);
+      }
+      
+    }
+  }
+  cartIconQty();
 
 // Product and Set Card Creation
 let setProductCard = function(result, destinationId) {
@@ -101,4 +137,153 @@ let topBannerStart = function(state, message) {
     }
     main.append(topBanner);
     setTimeout(topBannerHide, 10000);
+}
+
+// Homepage Card Creation
+let homepageCards = function(result, destination) {
+    var card = document.createElement("a");
+    card.setAttribute("class", `card ${result._type}-card`);
+
+    //Create Image
+    var cardImage = document.createElement("img");
+    cardImage.src = result.image;
+    card.append(cardImage);
+
+  //Create card text div
+    var cardDiv = document.createElement("div");
+    card.append(cardDiv);
+
+    //Create Primary Set or Collection Label
+    if(result.primary) {
+        var cardPrimary = document.createElement("a");
+        var styledPrimary = result.primary.title.replace("\"", "<b>\"");
+        styledPrimary = replaceLast("\"", "\"</b>", styledPrimary)
+        cardPrimary.setAttribute("class", "pill")
+        cardPrimary.innerHTML = styledPrimary;
+        cardPrimary.href = `/${result.primary._type}.html?id=${result.primary.slug.current}`
+        cardDiv.append(cardPrimary);
+    }
+
+    //Create Title
+    var cardTitle = document.createElement("h4");
+    cardTitle.innerHTML = result.title;
+    cardDiv.append(cardTitle);
+
+    //Create Minimum Price
+    if(result._type == 'product') {
+        var cardPrimary = document.createElement("p");
+        cardPrimary.innerHTML = '$' + result.store.priceRange.minVariantPrice.toFixed(2);
+        cardDiv.append(cardPrimary);
+    }
+
+    //Create link
+    card.href = `/${result._type}.html?id=${result.slug}`;
+
+    destination.append(card);
+}
+
+//Get Featured Products for the homepage
+let homeFeaturedProducts = function() {
+    var cards = document.getElementById("featured-products");
+    let query = encodeURIComponent(`[_id in ["c6a2ffaf-1b00-49f4-9469-98c2085045f0", "shopifyProduct-7534191050939", "shopifyProduct-7534191444155", "shopifyProduct-7559788626107"]] {${setAndProductProjection}}`);
+    sanityApiCall(query).then(() => {
+        sanityPromise.forEach((item)=>{
+            homepageCards(item, cards)
+        });
+        $(document).ready(function(){
+            $('#featured-products').slick({
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                swipeToSlide: true,
+                dots: false,
+                arrows: true,
+                centerMode: false,
+                focusOnSelect: false,
+                responsive:[
+                    {
+                        breakpoint: 680,
+                        settings: {
+                            arrows: false,
+                            slidesToShow: 1
+                        }
+                    }]
+            });
+        });
+    });
+    
+}
+
+//Change house geek images for mobile devices (on load)
+let houseGeekImages = function() {
+    var houseImages = document.getElementsByClassName("hg-image");
+    if (window.visualViewport.width <= 680) {
+        for (const houseImage of houseImages) {
+            houseImage.src = houseImage.src.replace("-feet.png", ".png");
+          }
+    }
+}
+
+// Store Page slider function
+let storeSliders = function(collectionName, targetDiv) {
+    let query = encodeURIComponent(`[_type == 'collection'] {'sets': sets[].set->{title, _type, 'slug': slug.current, 'image': image.asset->.url}}`);
+        sanityApiCall(query).then(res => {
+        for (const set of sanityPromise.sets){ 
+            setProductCard(set,targetDiv);
+        }
+        $(document).ready(function(){
+            $(`#${targetDiv}`).slick({
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                swipeToSlide: true,
+                dots: false,
+                arrows: true,
+                centerMode: false,
+                focusOnSelect: false,
+                responsive:[
+                    {
+                        breakpoint: 680,
+                        settings: {
+                            arrows: false,
+                            slidesToShow: 1
+                        }
+                    }]
+            });
+        });
+    });
+    
+}
+
+let collectionData = function() {
+    let query = encodeURIComponent(`[_type == 'collection' && slug.current == '${params.id}'] {title, description, 'image': image.asset->.url, 'featuredSet': featuredSet.set->{title, 'image': image.asset->.url, description, 'slug': slug.current}, theme, 'slug': slug.current, 'products': products[].product->{'price': store.priceRange.minVariantPrice, 'slug': store.slug.current, 'tags': store.tags, 'title': store.title, _type, 'image': store.previewImageUrl}, 'sets': sets[].set->{title, _type, 'slug': slug.current, 'image': image.asset->.url}}
+    `);
+    sanityApiCall(query)
+    .then(() => {
+        // Featured Set Data
+        let posters = document.getElementsByClassName('poster');
+        for (const poster of posters) {
+            poster.innerHTML = `<img src="${sanityPromise.featuredSet.image}"><h3>${sanityPromise.featuredSet.title}</h3>`;
+        }
+        let featuredSetInfo = document.getElementById('featured-set-text');
+        featuredSetInfo.innerHTML = `<h2>Featured Set</h2>
+            <h1 id="featured-title">${sanityPromise.featuredSet.title}</h1>
+            <p id="featured-description">${sanityPromise.featuredSet.description}</p>
+            <a class="striped-button" href="/set.html?id=${sanityPromise.featuredSet.slug}">View Set</a>`;
+        
+        // Collection info
+        let collectionRow = document.getElementById('collection-row');
+        collectionRow.innerHTML = `<div class="inset-row" style="display: block">
+            <img src="${sanityPromise.image}">
+            <p>${sanityPromise.description}</p>`;
+        collectionRow.setAttribute('style', `background: url("/images/background-overlay.png"), var(--${sanityPromise.theme});`);
+
+        // Sets
+        for (const set of sanityPromise.sets){
+            setProductCard(set, 'sets')
+        }
+
+        // Products
+        for (const product of sanityPromise.products){
+            setProductCard(product, 'products')
+        }
+    });
 }
--- static/js/shopify.js
+++ static/js/shopify.js
@@ -46,40 +46,6 @@ async function handleCart(payload, saveData) {
     }
   }
 
-// Cart structure check
-let cartStructureCheck = function(cartObject){
-    if(cartObject.lines.edges){
-      cartRoot = cartObject.lines.edges;
-    } else if (cartObject.lines.nodes){
-      cartRoot = cartObject.lines.nodes;
-    } else {
-      topBannerStart('error','There is an error in your cart, please clear your browser\'s cache and try again');
-    }
-  };
-  
-  if(sessionStorage.libralCart){
-    libralCart = JSON.parse(sessionStorage.libralCart);
-    cartStructureCheck(libralCart);
-  };
-  
-// Cart Icon Quantity display
-function cartIconQty() {
-    if(libralCart) {
-      if(document.getElementById("cart-icon-quantity")){
-        var cartQty = document.getElementById("cart-icon-quantity")
-        cartQty.innerHTML = cartRoot.length;
-      } else {
-        var cartIcon = document.getElementById('cart-icon');
-        var cartQty = document.createElement('p');
-        cartQty.setAttribute("id", "cart-icon-quantity");
-        cartQty.innerHTML = cartRoot.length;
-        cartIcon.append(cartQty);
-      }
-      
-    }
-  }
-  cartIconQty();
-
 //Stepper function script
 function decrement(e) {
     var value = e.target.parentElement.children[1].value = e.target.parentElement.children[1].value
@@ -196,7 +162,7 @@ function cartContentsFill() {
                     let lineDiv = document.createElement('div');
                     let price = Number(line.node.merchandise.price.amount).toFixed(2);
                     lineDiv.setAttribute('class', 'cart-line');
-                    lineDiv.setAttribute('id', line.node.merchandise.product.id.substring(22));
+                    lineDiv.setAttribute('title', line.node.merchandise.product.id.substring(22));
                     lineDiv.innerHTML = `
                         <a href="#">
                             <img src="${line.node.merchandise.image.url}">
@@ -236,55 +202,42 @@ function cartContentsFill() {
     }
 
     // Adding URLs to product page for each cart item
+    function matchSlugToId (){
+        return
+    }
+
     function cartLineUrls(){
+        console.log(shopifyIdArray);
         let ids = shopifyIdArray.join('", "')
         let query = encodeURIComponent(`[_id in ["${ids}"]] {'shopifyId': store.id, 'slug': store.slug.current}`);
         sanityApiCall(query).then(() => {
             var lines = document.getElementsByClassName('cart-line');
             shopifyIdArray.forEach(function(id, i){
-                lines[i].childNodes[1].href = `/product.html?id=${sanityPromise[i].slug}`;
+                let match = sanityPromise.filter(item => 'shopifyProduct-' + item.shopifyId == id)
+                console.log(match)
+                lines[i].childNodes[1].href = `/product.html?id=${match[0].slug}`;
             });
         })
     }
 }
 
-// Add product to cart    <-------------------------------------------------- CART SCRIPT START
-let selectedOptions = [];
-
 function findItem(value1, value2, value3) {
-  if(selectedOptions.length == 0){
-    return sanityPromise[0].variants
-  } else if(selectedOptions.length == 1){
-    return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1))
-  } else if(selectedOptions.length == 2){
-    return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1) && Object.values(variant.store).includes(value2))
-  } else if(selectedOptions.length == 3){
-    return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1) && Object.values(variant.store).includes(value2) && Object.values(variant.store).includes(value3))
+    if(selectedOptions.length == 0){
+      return sanityPromise[0].variants
+    } else if(selectedOptions.length == 1){
+      return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1))
+    } else if(selectedOptions.length == 2){
+      return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1) && Object.values(variant.store).includes(value2))
+    } else if(selectedOptions.length == 3){
+      return sanityPromise[0].variants.filter(variant => Object.values(variant.store).includes(value1) && Object.values(variant.store).includes(value2) && Object.values(variant.store).includes(value3))
+    }
   }
-}
+
 
 let successMessage;
 
-function addToCart(event){
-// Match selected options to variant ID
-  event.preventDefault();
-  const productOptions = document.forms.productOptions;
-  selectedOptions = [];
-  const formData = new FormData(productOptions);
-  let quantity = new Number;
-  for (const [key, value] of formData.entries()) {
-    if(key != 'quantity') {
-      selectedOptions.push(value);}
-    else if(key == 'quantity') {
-      quantity = parseInt(value)
-    }
-  }
-  let selectedVariant = findItem(selectedOptions[0], selectedOptions[1], selectedOptions[2])
-  let selectedVariantId = 'gid://shopify/ProductVariant/' + selectedVariant[0].store.id;
-  
-
-  if(!libralCart) {
-  // Create new cart & add line
+// Create cart with first item
+function createCart(selectedVariantId, quantity) {
     successMessage = `Your cart has been started.`;
     const query = `
       mutation cartCreate($input: CartInput) {
@@ -335,22 +288,11 @@ function addToCart(event){
     };
 
         handleCart(payload, 'data.data.cartCreate.cart');
-    
-  } else {
-  // If Cart already exists  
-    function generateCartLine() {
-      cartLine = undefined;
-      cartRoot.forEach(line => {
-        if((line.node && line.node.merchandise.id === selectedVariantId) || (line.merchandise && line.merchandise.id === selectedVariantId)) {
-          cartLine = line;
-        }
-      });
-    }
-    generateCartLine();
-    console.log('cart exists')
-    // Add line to cart
-    if(!cartLine) {
-      successMessage = 'Item has been added to your cart.';
+}
+
+// Add new product to cart
+function addCartLine(selectedVariantId, quantity){
+    successMessage = 'Item has been added to your cart.';
       const query = `
       mutation cartLinesAdd($cartId: ID!, $lines: [CartLineInput!]!) {
         cartLinesAdd(cartId: $cartId, lines: $lines) {
@@ -396,15 +338,16 @@ function addToCart(event){
         }
       }
       `;
-      const payload = {
+    const payload = {
         query: query,
         variables: { cartId: libralCart.id, lines: [{merchandiseId: selectedVariantId, quantity: quantity}]}
-      };
-      handleCart(payload, 'data.data.cartLinesAdd.cart');
-    
-    // Update existing line  
-    } else {
-      successMessage = 'Item quantity has been updated';
+    };
+    handleCart(payload, 'data.data.cartLinesAdd.cart');
+}
+
+// Updated quantity of item in cart
+function updateCartLine(selectedVariantId, quantity){
+    successMessage = 'Item quantity has been updated';
       const query = `	mutation cartLinesUpdate($cartId: ID!, $lines: [CartLineUpdateInput!]!) {
         cartLinesUpdate(cartId: $cartId, lines: $lines) {
           cart {
@@ -456,9 +399,4 @@ function addToCart(event){
           }
       };
       handleCart(payload, 'data.data.cartLinesUpdate');
-    }
-  }
-};
-
-
-productOptions.addEventListener('submit', addToCart);
+}
